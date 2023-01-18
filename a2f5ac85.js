6bdb7824f2c4d5d75d16b2ca4facd3c9ac3d3edf
JWerbrouck <33063548+JWerbrouck@users.noreply.github.com>
2023-01-18 15:08:53 +0000

    Deploying to gh-pages from @ AECOstore/validator-module@91c7c651e36b56481b058cd2b4fa631d13751a6e 🚀

---
+++ a2f5ac85.js
@@ -0,0 +1,27 @@
+"use strict";
+(self["webpackChunkpr_querymodule"] = self["webpackChunkpr_querymodule"] || []).push([["src_App_tsx"],{
+
+/***/ "./src/App.tsx":
+/*!*********************!*\
+  !*** ./src/App.tsx ***!
+  \*********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "react");
+
+
+var App = function App(_ref) {
+  var piral = _ref.piral;
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", null, "I am a validator API");
+};
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (App);
+
+/***/ })
+
+}]);
+//# sourceMappingURL=a2f5ac85.js.map
